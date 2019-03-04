16c44297f19f75f87098e70a6a3166561eea73f7
github username <github email address>
2019-03-04 14:02:59 -0800

    created with Reid

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hi! What's your favorite Starwars character?!?!?!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  char_name = STDIN.gets.chomp.downcase
 end
